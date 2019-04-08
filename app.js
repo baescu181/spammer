5564e431ecba07edef9d37ea75900b80de2cfde0
baescu181 <33103781+baescu181@users.noreply.github.com>
2019-04-08 20:22:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,12 +28,3 @@ bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
 bot.login(config.token);
-
-
-function clear() {
-    console.clear();
-    console.log(figlet.textSync("MassDM v3.2.0").green);
-    console.log("\n\nMass DM bot for Discord. Credit to Gringo(Scammer ALT)!\nSends DMs to selected members of guild.");
-    console.log(`\nForked and improved by _4Smile <3.\n Random send time set @ 0.01-${config.wait}s`);
-    console.log(`Type ${config.prefix}help in a chat.\n\n`);
-}
